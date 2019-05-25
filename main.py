4c4f1ed9e7f60ec2b9f5a0b929c6e55f9e45e697
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-05-25 19:24:54 +0300

    split recognition from screenshot and file into 2 func

--- main.py
+++ main.py
@@ -1,20 +1,50 @@
 import os
+import time
 from dac.ocr import recognize_heroes_on_image, recognize_heroes_on_image_file
 from util.sreenshooter import screenshot
-import time
 
-if __name__ == '__main__':
-    heroes_in_file = {}
 
-    # recognize from screenshot
-    time.sleep(1)
-    screen = screenshot((280, 365, 1350+280, 35+365))
-    heroes = recognize_heroes_on_image(screen, show=True)
-    print(heroes)
+def main():
+    """ recognize from screenshot """
+    heroes_chart = {}
 
-    # recognize from files
+    prev_heroes = []
+    while True:
+        time.sleep(1)
+        screen = screenshot((280, 365, 1350+280, 35+365))
+        heroes = recognize_heroes_on_image(screen, show=False)
+        if sum([hero[1] for hero in heroes]) == 5 and prev_heroes != heroes:
+            # print(heroes)
+            for hero in heroes:
+                if heroes_chart.get(hero[0]) is None:
+                    heroes_chart[hero[0]] = hero[1]
+                else:
+                    heroes_chart[hero[0]] += hero[1]
+            print(heroes_chart)
+            prev_heroes = heroes
+
+
+def test():
+    """ recognize from files """
+    heroes_chart = {}
     for img in os.listdir('./images'):
         heroes = recognize_heroes_on_image_file('./images/' + img, show=False)
         print(img, heroes)
-        heroes_in_file[img] = heroes
+
+        for hero in heroes:
+            if heroes_chart.get(hero[0]) is None:
+                heroes_chart[hero[0]] = hero[1]
+            else:
+                heroes_chart[hero[0]] += hero[1]
+
+    print(heroes_chart)
+
+
+if __name__ == '__main__':
+    main()
+    # test()
+
+
+
+
 
