546a02012111abe684b217bd7b33400906b0d0c9
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-05-31 19:54:52 +0300

    remove comments

--- dac/cnn/cifar_nn.py
+++ dac/cnn/cifar_nn.py
@@ -8,7 +8,6 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.optimizers import SGD
 
-# Задаем seed для повторяемости результатов
 np.random.seed(42)
 labels = []
 img_names = []
@@ -48,44 +47,31 @@ nb_epoch = 30
 Y_train = np_utils.to_categorical(Y_train, nb_classes)
 
 model = Sequential()
-# Первый сверточный слой
 model.add(Conv2D(32, (3, 3), padding='same', input_shape=(40, 32, 3), activation='relu'))
-# Второй сверточный слой
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-# Первый слой подвыборки
 model.add(MaxPooling2D(pool_size=(2, 2)))
-# Слой регуляризации Dropout
 model.add(Dropout(0.25))
 
-# Третий сверточный слой
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
-# Четвертый сверточный слой
 model.add(Conv2D(64, (3, 3), activation='relu'))
-# Второй слой подвыборки
 model.add(MaxPooling2D(pool_size=(2, 2)))
-# Слой регуляризации Dropout
 model.add(Dropout(0.25))
-# Слой преобразования данных из 2D представления в плоское
+
 model.add(Flatten())
-# Полносвязный слой для классификации
 model.add(Dense(512, activation='relu'))
-# Слой регуляризации Dropout
 model.add(Dropout(0.5))
-# Выходной полносвязный слой
 model.add(Dense(nb_classes, activation='softmax'))
 
-# Задаем параметры оптимизации
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
-# Обучаем модель
+
 model.fit(X_train, Y_train,
               epochs=nb_epoch,
               shuffle=True,
               verbose=2)
 
-# Оцениваем качество обучения модели на тестовых данных
 scores = model.evaluate(X_train, Y_train, verbose=0)
 print("Точность работы на тестовых данных: %.2f%%" % (scores[1]*100))
 
