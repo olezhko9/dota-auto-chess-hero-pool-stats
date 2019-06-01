25bf34852ca387949d4add07354a0536a54962e0
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-06-01 16:13:56 +0300

    image croper returns thumbnails

--- dac/cnn/hero_img_croper.py
+++ dac/cnn/hero_img_croper.py
@@ -1,9 +1,9 @@
 from PIL import Image
-from dac.ocr import recognize_heroes_on_image_file
+from dac.ocr import recognize_heroes_on_image
 import os
 
 
-def crop_heroes(images_path):
+def crop_heroes(im):
     crop_config = {
         'x': 285,
         'y': 150,
@@ -12,28 +12,31 @@ def crop_heroes(images_path):
         'h': 200,
     }
 
+    heroes = recognize_heroes_on_image(im, show=False, need_crop=True)
+    cropped_heroes = []
+
+    if len(heroes) == 5:
+        for i in range(5):
+            hero_crop = im.crop((crop_config['x'] + crop_config['margin'] * (i + 1) + crop_config['w'] * i,
+                                 crop_config['y'],
+                                 crop_config['x'] + (crop_config['margin'] + crop_config['w']) * (i + 1),
+                                 crop_config['y'] + crop_config['h']
+                                 ))
+
+            hero_crop.thumbnail((36, 40))
+            cropped_heroes.append((heroes[i], hero_crop))
+    return cropped_heroes
+
+
+if __name__ == '__main__':
+    images_path = '../../images/'
     unique_hero_img_count = 0
     for filename in os.listdir(images_path):
         filename = images_path + filename
         if os.path.isdir(filename):
             continue
-        heroes = recognize_heroes_on_image_file(filename, show=False)
-        # print(heroes)
-
-        if len(heroes) == 5:
-            im = Image.open(filename)
-
-            for i in range(5):
-                hero_crop = im.crop((crop_config['x'] + crop_config['margin'] * (i + 1) + crop_config['w'] * i,
-                                     crop_config['y'],
-                                     crop_config['x'] + (crop_config['margin'] + crop_config['w']) * (i + 1),
-                                     crop_config['y'] + crop_config['h']
-                                     ))
-
-                hero_crop.thumbnail((36, 40))
-                hero_crop.save(images_path + "heroes/" + heroes[i] + " - " + str(unique_hero_img_count) + ".png")
+        else:
+            hero_crop = crop_heroes(Image.open(filename))
+            for hc in hero_crop:
+                hc[1].save(images_path + "heroes/" + hc[0] + " - " + str(unique_hero_img_count) + ".png")
                 unique_hero_img_count += 1
-
-
-if __name__ == '__main__':
-    crop_heroes('../../images/')
